6719d594d9188547d0f6dc977b5c225debbdfae5
Amber-Lee Madigan <amadigan@mediatemple.net>
2016-10-06 09:41:01 -0700

    :bug: removed token from code

--- src/ladyBossBot.js
+++ src/ladyBossBot.js
@@ -64,10 +64,10 @@
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 
-// if (!process.env.TOKEN_SLACK) {
-//     console.log('Error: Specify token in environment');
-//     process.exit(1);
-// }
+if (!process.env.TOKEN_SLACK) {
+    console.log('Error: Specify token in environment');
+    process.exit(1);
+}
 
 var Botkit = require('../lib/Botkit.js');
 var os = require('os');
@@ -80,8 +80,7 @@ var controller = Botkit.slackbot({
 
 
 var bot = controller.spawn({
-  // token: process.env.token
-  token: "xoxb-42902998438-Ey4Z0ytirlyg9zGHwdF2rRjq"
+  token: process.env.token
 }).startRTM();
 
 
